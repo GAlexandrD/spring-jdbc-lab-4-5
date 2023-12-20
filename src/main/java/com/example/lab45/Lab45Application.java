968e3a5edceb4df10e8d4b64f425d3d10d930eb0
suxarik <suxarik312004gor@gmail.com>
2023-12-20 10:09:28 +0200

    setup new project

---
+++ src/main/java/com/example/lab45/Lab45Application.java
@@ -0,0 +1,13 @@
+package com.example.lab45;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab45Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab45Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lab45/Lab45ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab45;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab45ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
