f908d44b2681cfe03c8ebf3fd7514dc83b7a314a
suxarik <suxarik312004gor@gmail.com>
2023-12-21 15:26:39 +0200

    Add OpenApi Definition

--- src/main/java/com/example/lab45/Lab45Application.java
+++ src/main/java/com/example/lab45/Lab45Application.java
@@ -1,10 +1,27 @@
 package com.example.lab45;
 
 import com.example.lab45.Task.utils.DateParser;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+@OpenAPIDefinition(
+		info = @Info(
+
+				title = "Task Manager API",
+				description = "This is project for Spring labs"
+
+		),
+		servers = {
+
+				@Server(url = "http://localhost:8080", description = "test server"),
+				@Server(url = "http://example.com", description = "production server")
+		}
+
+)
 @SpringBootApplication
 public class Lab45Application {
 
